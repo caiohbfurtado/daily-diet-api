2373c6aa2a4865764ef683ea55f426ad389df401
Caio <caio.furtado@compasso.com.br>
2024-06-10 10:21:29 -0300

    feat: connect with local database and add types

---
+++ db/migrations/20240610131256_create-meals.ts
@@ -0,0 +1,16 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id').primary()
+    table.uuid('session_id').after('id').index()
+    table.string('name').notNullable()
+    table.string('description').notNullable()
+    table.boolean('within_diet').notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals')
+}
--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from 'fastify'
+import { knex } from '../database'
 
 export async function mealsRoutes(app: FastifyInstance) {
-  app.get('/', () => {
-    return 'meals teste'
+  app.get('/', async () => {
+    return await knex('meals').select()
   })
 }
